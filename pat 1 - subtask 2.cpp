e61e71d4e1c3b66fa3f8fb5167e04b2e3c8ddab2
PALESA  ABEYUDI ZITHA <palesazitha2002@gmail.com>
2025-02-25 13:43:13 +0200

    Update pat 1 - subtask 2.cpp
    i have changed the spelling errors and i fixed the code in order for the program can run

--- pat 1 - subtask 2.cpp
+++ pat 1 - subtask 2.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 
 using namespace std;
@@ -14,8 +15,7 @@ int main(){
 
   //checking if the second reading increases by more than 50 degrees
   if(temp2 - temp1 > 50){
-    cout << "Reduce fryer heat before taking the third reading.\n;
-      return 0;
+    cout << "Reduce fryer heat before taking the third reading.";
   }
 
 cout <<"Enter the third temperature reading:";
@@ -23,14 +23,15 @@ cin >> temp3;
 
 //Check if the increase between second and third readings is less than 10
 if(temp3 - temp2 < 10) {
-  cout << "increase the fryer heat before taking the third reading.\n";
-    return 0;
+  cout << "increase the fryer heat before taking the third reading.";
+  
 }
 
 // Check if the final temeperature is within the accepatable range
   if (temp3 >= 150 && temp3 <= 190) {
-    cout <<"You may start frying the Magwinyas./n";
+    cout<<"You may start frying the Magwinyas";
      } else {
-        cout <<"Oil is not ready for frying!\n";
-    return o;
-  }
+        cout <<"Oil is not ready for frying!";
+}
+        return 0;
+}
