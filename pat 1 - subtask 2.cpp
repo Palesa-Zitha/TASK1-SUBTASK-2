dd9f5bd37c65733ed89cf87300e5b6aa21e84b55
PALESA  ABEYUDI ZITHA <palesazitha2002@gmail.com>
2025-02-25 09:26:22 +0200

    Create pat 1 - subtask 2.cpp

---
+++ pat 1 - subtask 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int temp1, temp2, temp3;
+
+  //Get temperature readings from the user
+  cout <<"Enter the first temeperature reading:";
+  cin >> temp1;
+
+  cout <<"Enter the second temeperature reading:";
+  cin >> temp2;
+
+  //checking if the second reading increases by more than 50 degrees
+  if(temp2 - temp1 > 50){
+    cout << "Reduce fryer heat before taking the third reading.\n"
+      return 0;
+  }
+
+cout <<"Enter the third temperature reading:";
+cin >> temp3;
+
+//Check if the increase between second and third readings is less than 10
+if(temp3 - temp2 < 10) {
+  cout << "increase the fryer heat before taking the third reading.\n"
+    return 0;
+}
+
+// Check if the final temeperature is within the accepatable range
+  if (temp3 >= 150 && temp3 <= 190) {
+    cout("You may start frying the Magwinyas./n";)
+     } else {
+        cout <<"Oil is not ready for frying!\n";
+  }
